48c9f3d7116401000b77ead16e1ba6f09a3e96ef
Alois~ <moaugustin@proton.me>
2025-07-28 21:34:05 +0200

    Adjusted Upload Size

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ let userCreations = {};
 
 // Environment
 app.use(cors());
-app.use(express.json({ limit: "12mb" }));
-app.use(express.urlencoded({ limit: "12mb", extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 // API Endpoints
 app.get('/api/register/init', async (req, res) => {
