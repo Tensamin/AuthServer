78eb6381e866b132813293ee1f9070c238745dab
Aloisianer <moaugustin@proton.me>
2025-08-23 11:03:50 +0200

    Debugging

--- app.js
+++ app.js
@@ -341,6 +341,7 @@ app.post('/api/register/options/:uuid', async (req, res) => {
     let uuid = req.params.uuid;
 
     try {
+        console.log(db)
         let user = await db.get(uuid);
         if (req.body.private_key_hash === user.private_key_hash) {
             let options = await generateRegistrationOptions({
