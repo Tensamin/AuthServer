bf1848a1edc4f5f6cf4adf80b73ded8214638866
Aloisianer <moaugustin@proton.me>
2025-08-24 17:29:12 +0200

    weew

--- app.js
+++ app.js
@@ -15,32 +15,10 @@ let userCreations = [];
 let rpID = process.env.RPID || 'tensamin.methanium.net';
 let rpName = 'Tensamin';
 let primaryOrigin = process.env.ORIGIN || "https://tensamin.methanium.net";
-let allowedOrigins = new Set([primaryOrigin, 'app://-', 'null']);
-
-let corsOptions = {
-    origin: (incomingOrigin, callback) => {
-        try {
-            if (!incomingOrigin || allowedOrigins.has(incomingOrigin)) {
-                return callback(null, true);
-            }
-
-            return callback(
-                new Error(
-                    `Not allowed by CORS policy for origin: ${String(incomingOrigin)}`
-                ),
-                false
-            );
-        } catch (err) {
-            return callback(err, false);
-        }
-    },
-    credentials: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type,Authorization',
-};
+let allowedOrigins = [primaryOrigin, 'app://-'];
 
 // Environment
-app.use(cors(corsOptions));
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 app.use(express.json({ limit: "16mb" }));
 app.use(express.urlencoded({ extended: true, limit: "16mb" }));
 
