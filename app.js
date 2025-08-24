52c0d7c37f647f5408c26b6e8bc1b57c5601a1ca
Aloisianer <moaugustin@proton.me>
2025-08-24 17:29:44 +0200

    wee

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ let primaryOrigin = process.env.ORIGIN || "https://tensamin.methanium.net";
 let allowedOrigins = [primaryOrigin, 'app://-'];
 
 // Environment
-app.use(cors({ origin: allowedOrigins, credentials: true }));
+app.use(cors({ credentials: true }));
 app.use(express.json({ limit: "16mb" }));
 app.use(express.urlencoded({ extended: true, limit: "16mb" }));
 
